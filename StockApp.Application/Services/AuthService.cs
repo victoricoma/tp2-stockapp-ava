1e6599ffe803df9bfb1857aa3c0f05a6f1ae2b0d
shavrau <rhian.souza@fatec.sp.gov.br>
2024-06-20 00:10:42 -0300

    serviço de autenticação que gerará o token JWT

---
+++ StockApp.Application/Services/AuthService.cs
@@ -0,0 +1,54 @@
+﻿using StockApp.Application.DTOs;
+using StockApp.Application.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using StockApp.Domain.Interfaces;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace StockApp.Application.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IAppConfiguration _configuration;
+
+        public AuthService(IUserRepository userRepository, IAppConfiguration configuration)
+        {
+            _userRepository = userRepository;
+            _configuration = configuration;
+        }
+
+        public async Task<TokenResponseDTO> AuthenticateAsync(string username, string password)
+        {
+            var user = await _userRepository.GetByUsernameAsync(username);
+            if (user == null || !BCrypt.Net.BCrypt.Verify(password, user.PasswordHash))
+            {
+                return null;
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(double.Parse(_configuration["Jwt:ExpireMinutes"])),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return new TokenResponseDTO
+            {
+                Token = tokenHandler.WriteToken(token)
+            };
+        }
+    }
+}
