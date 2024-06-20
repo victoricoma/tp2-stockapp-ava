1128e38f1d3c65fab505407bb4d61100b0cb2e64
shavrau <rhian.souza@fatec.sp.gov.br>
2024-06-20 00:27:55 -0300

    Add TokenController

---
+++ StockApp.API/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.Interfaces;
+using StockApp.Application.DTOs;
+
+namespace StockApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public TokenController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDTO userLoginDTO)
+        {
+            var token = await _authService.AuthenticateAsync(userLoginDTO.Username, userLoginDTO.Password);
+
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(token);
+        }
+    }
+}
