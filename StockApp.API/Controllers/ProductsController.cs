e5bf67552d03a7236aea3c59c13ffa63708fb247
pedrobiondi23 <pedro.carvalho32@fatec.sp.gov.br>
2024-06-20 12:02:05 -0300

    Adicionar Relatório de Estoque Baixo

---
+++ StockApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Domain.Entities;
+using StockApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productRepository.GetProducts();
+            return View(products);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productRepository.Create(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _productRepository.Update(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _productRepository.GetById(id);
+            if (product != null)
+            {
+                await _productRepository.Remove(product);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("low-stock")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetLowStock([FromQuery] int threshold)
+        {
+            var products = await _productRepository.GetLowStockAsync(threshold);
+            return Ok(products);
+        }
+    }
+}
--- StockApp.Infra.Data/Repositories/ProductRepository.cs
+++ StockApp.Infra.Data/Repositories/ProductRepository.cs
@@ -2,53 +2,93 @@
 using StockApp.Domain.Interfaces;
 using StockApp.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace StockApp.Infra.Data.Repositories
 {
     public class ProductRepository : IProductRepository
     {
-        ApplicationDbContext _productContext;
+        private readonly ApplicationDbContext _context;
+
         public ProductRepository(ApplicationDbContext context)
         {
-            _productContext = context;
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Product>> GetLowStockAsync(int threshold)
+        {
+            return await _context.Products
+                .Where(p => p.Stock < threshold)
+                .ToListAsync();
         }
 
         public async Task<IEnumerable<Product>> GetAllAsync(int pageNumber, int pageSize)
         {
-            return await _productContext.Products
-                .Skip(pageNumber).Take(pageSize)
+            return await _context.Products
+                .Skip(pageNumber * pageSize)
                 .Take(pageSize)
                 .ToListAsync();
         }
 
         public async Task<Product> Create(Product product)
         {
-            _productContext.Add(product);
-            await _productContext.SaveChangesAsync();
+            if (product == null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+
+            _context.Add(product);
+            await _context.SaveChangesAsync();
             return product;
         }
 
         public async Task<Product> GetById(int? id)
         {
-            return await _productContext.Products.FindAsync(id);
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id), "Id cannot be null.");
+            }
+
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                throw new KeyNotFoundException($"Product with id {id} not found.");
+            }
+
+            return product;
         }
 
         public async Task<IEnumerable<Product>> GetProducts()
         {
-            return await _productContext.Products.ToListAsync();
+            return await _context.Products.ToListAsync();
         }
 
         public async Task<Product> Remove(Product product)
         {
-            _productContext.Remove(product);
-            await _productContext.SaveChangesAsync();
+            if (product == null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+
+            _context.Remove(product);
+            await _context.SaveChangesAsync();
             return product;
         }
 
         public async Task<Product> Update(Product product)
         {
-            _productContext.Update(product);
-            await _productContext.SaveChangesAsync();
+            if (product == null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+
+            _context.Update(product);
+            await _context.SaveChangesAsync();
             return product;
         }
     }
