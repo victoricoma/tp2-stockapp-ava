91a603a412526bf3fa5d9d9688c095868311d632
kamilysimeao <simeaokamily@gmail.com>
2024-06-20 23:12:26 -0300

    Criação do Controlador e Endpoints de Leitura (GetAll e GetById)

---
+++ StockApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Domain.Entities;
+using StockApp.Domain.Interfaces;
+using StockApp.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockApp.Application.DTOs;
+
+namespace StockApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+       
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+         
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var products = await _productRepository.GetProducts();
+            if (products == null)
+            {
+                return NotFound("Products not found");
+            }
+            return Ok(products);
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> Get(int id)
+        {
+            var product = await _productRepository.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound("Product not found");
+            }
+            return Ok(product);
+        }
+
+      
+    }
+}
