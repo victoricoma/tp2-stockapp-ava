80c28d24a0176df0f1a9b177cc473b19699552d5
pedrobiondi23 <pedro.carvalho32@fatec.sp.gov.br>
2024-06-21 14:50:35 -0300

    Implementar Autenticação Multi-Fator (MFA)

---
+++ StockApp.API/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.Interfaces;
+using System;
+
+namespace StockApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMfaService _mfaService;
+
+        public AuthController(IMfaService mfaService)
+        {
+            _mfaService = mfaService;
+        }
+
+        [HttpPost("generate-otp")]
+        public ActionResult<string> GenerateOtp()
+        {
+            var otp = _mfaService.GenerateOtp();
+            return Ok(otp);
+        }
+
+        [HttpPost("validate-otp")]
+        public ActionResult<bool> ValidateOtp([FromBody] string userOtp)
+        {
+            string storedOtp;
+            var isValid = _mfaService.ValidateOtp(userOtp, out storedOtp);
+            return Ok(isValid);
+        }
+    }
+}
---
+++ StockApp.Application/DTOs/IMfaService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StockApp.Application.Interfaces
+{
+    public interface IMfaService
+    {
+        string GenerateOtp();  
+        bool ValidateOtp(string userOtp, out string storedOtp);  
+    }
+}
+
