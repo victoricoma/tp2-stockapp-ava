68f3db4f13031f16e168e05186abb9efa35bc289
shavrau <rhian.souza@fatec.sp.gov.br>
2024-06-21 09:40:14 -0300

    Enhanced UsersController with logging and tests
    - Integrated ILogger<UsersController> for logging in UsersController, improving error handling and information tracking during user registration.
    - Modified UsersController constructor to support dependency injection of ILogger alongside IUserRepository, enhancing the controller's functionality.
    - Added comprehensive validation in the Register method to ensure user data integrity, including checks for null or whitespace in username and password fields, improving data validation and error response.
    - Implemented UsersControllerTests with two test methods to validate the Register method's behavior with both valid and invalid user data, ensuring reliability and correctness of user registration process.
    - Included necessary using directives in UsersController and UsersControllerTests for logging, mocking, asynchronous operations, and unit testing, streamlining development and testing processes.

--- StockApp.API/Controllers/UsersController.cs
+++ StockApp.API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using StockApp.Domain.Entities;
 using StockApp.Domain.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using StockApp.Application.DTOs;
+using Microsoft.Extensions.Logging;
 
 namespace StockApp.API.Controllers
 {
@@ -12,10 +13,12 @@ namespace StockApp.API.Controllers
     public class UsersController : ControllerBase
     {
         private readonly IUserRepository _userRepository;
+        private readonly ILogger<UsersController> _logger;
 
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, ILogger<UsersController> logger)
         {
             _userRepository = userRepository;
+            _logger = logger;
         }
 
         [Authorize(Policy = "adminPolicy")]
@@ -24,7 +27,20 @@ namespace StockApp.API.Controllers
         {
             if (userRegisterDto == null)
             {
+                _logger.LogError("Invalid user data received.");
                 return BadRequest("Invalid user data.");
+            }
+
+            if (string.IsNullOrWhiteSpace(userRegisterDto.Username))
+            {
+                _logger.LogError("Invalid username.");
+                return BadRequest("Username cannot be empty.");
+            }
+
+            if (string.IsNullOrWhiteSpace(userRegisterDto.Password))
+            {
+                _logger.LogError("Invalid password.");
+                return BadRequest("Password cannot be empty.");
             }
 
             var user = new User
@@ -35,6 +51,9 @@ namespace StockApp.API.Controllers
             };
 
             await _userRepository.AddAsync(user);
+
+            _logger.LogInformation($"User {user.Username} registered successfully.");
+
             return Ok();
         }
     }
