5c3f453c7f78d7931b4bf3394fa8772873b25e48
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2024-06-22 15:49:38 -0300

    configuração-tokens

---
+++ StockApp.Application/Products/Handlers/ProductRemoveCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using StockApp.Application.Products.Commands;
+using StockApp.Domain.Entities;
+using StockApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.Products.Handlers
+{
+    internal class ProductRemoveCommandHandler : IRequestHandler<ProductRemoveCommand, Product>
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductRemoveCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new
+                ArgumentNullException(nameof(productRepository));
+        }
+        public async Task<Product> Handle(ProductRemoveCommand request,
+       CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+
+            if (product == null)
+            {
+                throw new ApplicationException($"Entity could not be found.");
+            }
+            else
+            {
+                var result = await _productRepository.RemoveAsync(product);
+                return result;
+            }
+        }
+        public Task<Product> Handle(ProductRemoveCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
