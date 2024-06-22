038659a0b2484226090adb33fd6d82ca98613acc
FelipeMoretti <felipe.moretti@fatec.sp.gov.br>
2024-06-22 01:22:15 -0300

    Implementação do Serviço de Recomendação de Produtos

---
+++ StockApp.Infra.Data/Interfaces/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockApp.Domain.Entities;
+
+namespace StockApp.Infra.Data.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task<IEnumerable<Order>> GetByUserIdAsync(string userId);
+    }
+}
