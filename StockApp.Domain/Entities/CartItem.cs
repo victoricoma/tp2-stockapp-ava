22a8d6b0ff469cebf2ff49d0991b318b3218b97b
vicdsso <vicdsso@gmail.com>
2024-06-22 00:40:16 -0300

    Implementar Funcionalidade de Carrinho de Compras

---
+++ StockApp.Domain/Entities/CartItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Domain.Entities
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
