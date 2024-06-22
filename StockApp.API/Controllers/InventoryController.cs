3c2db467f6fed479f661f5ca093f3ce4186f3a2a
kayrocsilva <kayro.silva@fatec.sp.gov.br>
2024-06-22 03:31:46 -0300

    Implementar Gestão de Inventário Just-in-Time

---
+++ StockApp.API/Controllers/InventoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.Interfaces;
+
+namespace StockApp.API.Controllers
+{
+    public class InventoryController : ControllerBase
+    {
+        private readonly IJustInTimeInventoryService _jitInventoryService;
+
+        public InventoryController(IJustInTimeInventoryService jitInventoryService)
+        {
+            _jitInventoryService = jitInventoryService;
+        }
+
+        [HttpPost("optimize-inventory")]
+        public async Task<IActionResult> OptimizeInventory()
+        {
+            try
+            {
+                await _jitInventoryService.OptimizeInventoryAsync();
+                return Ok("Inventário otimizado com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro ao otimizar inventário: {ex.Message}");
+            }
+        }
+    }
+
+}
