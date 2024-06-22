cffd2e2191c0a8d15fa6133f3438ed5ab6e9ed44
Clestxn <cleston.tonooka@fatec.sp.gov.br>
2024-06-22 12:29:46 -0300

    Implementar Sistema de Análise de Tendências de Mercado

---
+++ StockApp.Application/Services/MarketTrendAnalysisService.cs
@@ -0,0 +1,21 @@
+﻿using StockApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.Services
+{
+    public class MarketTrendAnalysisService : IMarketTrendAnalysisService
+    {
+        public async Task<MarketTrendDto> AnalyzeTrendsAsync()
+        {
+            return new MarketTrendDto
+            {
+                Trend = "Aumento nas vendas de ingressos.",
+                Prediction = "Crescimento de 22% nas próximas semanas."
+            };
+        }
+    }
+}
---
+++ StockApp.Domain.Test/MarketTrendAnalysisServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using StockApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using StockApp.Application.Services;
+
+namespace StockApp.Domain.Test
+{
+    public class MarketTrendAnalysisServiceTests
+    {
+        private readonly IMarketTrendAnalysisService _marketTrendAnalysisService;
+
+        public MarketTrendAnalysisServiceTests()
+        {
+            _marketTrendAnalysisService = new MarketTrendAnalysisService();
+        }
+
+        [Fact]
+        public async Task AnalyzeTrendsAsync_ShouldReturnExpectedResult()
+        {
+            var result = await _marketTrendAnalysisService.AnalyzeTrendsAsync();
+
+            Assert.Equal("Aumento nas vendas de ingressos.", result.Trend);
+            Assert.Equal("Crescimento de 22% nas próximas semanas.", result.Prediction);
+        }
+    }
+}
