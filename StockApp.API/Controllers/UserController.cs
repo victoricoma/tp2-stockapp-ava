48465ba54478f9112693733638bb933061368189
kayrocsilva <kayro.silva@fatec.sp.gov.br>
2024-06-23 20:42:47 -0300

    Criação do Controlador UserController e Endpoint para Criar Usuário

---
+++ StockApp.API/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.DTOs;
+using StockApp.Domain.Entities;
+using StockApp.Domain.Interfaces;
+
+namespace StockApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegisterDTO userRegisterDTO)
+        {
+            // Validações básicas (podem ser expandidas conforme necessário)
+            if (userRegisterDTO == null || string.IsNullOrEmpty(userRegisterDTO.UserName) || string.IsNullOrEmpty(userRegisterDTO.Password))
+            {
+                return BadRequest("Invalid user data");
+            }
+
+            // Criação da entidade User
+            var user = new User
+            {
+                Username = userRegisterDTO.UserName,
+                PasswordHash = System.Text.Encoding.UTF8.GetBytes(userRegisterDTO.Password),
+                Role = userRegisterDTO.Role
+            };
+
+            // Adiciona o usuário ao repositório
+            await _userRepository.AddAsync(user);
+
+            return Ok("User registered successfully");
+        }
+    }
+}
---
+++ StockApp.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using StockApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(User user);
+    }
+}
