0b2c1133d7c7928806b3379142c12f5d636778e7
kamilyfatec <kamily.simeao@fatec.sp.gov.br>
2024-06-23 16:31:00 -0300

    Criar Classe Supplier no Domínio

---
+++ StockApp.Domain/Entities/Supplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Domain.Entities
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactEmail { get; set; }
+        public string PhoneNumber{ get; set; }
+    }
+}
