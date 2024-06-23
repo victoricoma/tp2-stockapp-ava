25241b65c96e2d1d9352b4f6aea8b226a761c1c0
kayrocsilva <kayro.silva@fatec.sp.gov.br>
2024-06-23 19:32:37 -0300

    Criação do Serviço de Autenticação

--- StockApp.Infra.Data/Identity/ApplicationUser.cs
+++ StockApp.Infra.Data/Identity/ApplicationUser.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace StockApp.Infra.Data.Identity
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
     }
 }
